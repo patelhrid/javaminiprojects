829d195d568e83cb0d842fd1be380e8e03f3920d
patelhrid <74324750+patelhrid@users.noreply.github.com>
2020-11-11 15:18:36 -0500

    Add files via upload

---
+++ MethodsExample.java
@@ -0,0 +1,41 @@
+package methodsexample;
+
+public class MethodsExample {
+
+    static void iPhone12Mini (){
+        
+        System.out.println("The iPhone 12 Mini is the smallest and cheapest new iPhone.");
+        System.out.println("It is also an easy recommendation for people who want to buy it.");
+        
+    }
+    
+    static void iPhone12 (){
+        
+        System.out.println("The iPhone 12 is the second biggest new iPhone.");
+        System.out.println("It is also an easy recommendation for people who want to buy a bigger phone.");
+        
+    }
+    
+    static void iPhone12Pro (){
+        
+        System.out.println("The iPhone 12 Pro is the second and biggest and second most expensive new iPhone.");
+        System.out.println("It is a tougher recommendation since it is similar to the 12");
+        
+    }
+    static void iPhone12ProMax (){
+        
+        System.out.println("The iPhone 12 Pro Max is the biggest new iPhone");
+        System.out.println("It is  an easy recommendation for people who want the best iPhone.");
+        
+    }
+   
+    public static void main(String[] args) {
+     
+    iPhone12();
+    iPhone12Pro();
+    iPhone12ProMax();
+    iPhone12Mini();
+        
+    }
+    
+}
