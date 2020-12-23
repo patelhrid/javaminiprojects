def02c4d279b9f6c98b5db025f3e177df07daba7
patelhrid <74324750+patelhrid@users.noreply.github.com>
2020-12-22 20:41:45 -0500

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,20 @@
+public class Boolean {
+  
+  
+  public static void main(String[] args) { 
+    
+    int a=4, b=5, c=6;
+    if (a<b && a<c)
+    {
+      System.out.println(a);
+    }
+    if (b<a && b<c)
+    {
+      System.out.println(c);
+    }
+    if (c<a && c<b)
+    {
+      System.out.println(c);
+    }
+  }
+}
