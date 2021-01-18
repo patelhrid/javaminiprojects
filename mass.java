4aeee8d46940b5e99b0891cbb22c92b4ca1de4c9
patelhrid <74324750+patelhrid@users.noreply.github.com>
2021-01-18 17:22:53 -0500

    Add files via upload

---
+++ mass.java
@@ -0,0 +1,10 @@
+class mass
+{
+  public static void main (String args[])
+  {
+    System.out.println("What is your weight?");
+    double myMass = In.getDouble();
+    System.out.println("Your weight is " + myMass);
+  }
+}
+    
