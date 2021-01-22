c3eabba4ec06e5aea02c892cdb54096479b17788
patelhrid <74324750+patelhrid@users.noreply.github.com>
2021-01-21 21:57:17 -0500

    Add files via upload

---
+++ printable.java
@@ -0,0 +1,10 @@
+class printable
+{
+  public static void main (String[] args)
+{
+System.out.print("If at first ");
+System.out.println("you don't succeed" + ",");
+System.out.print("failure may be ");
+System.out.println("your style");
+}
+}
