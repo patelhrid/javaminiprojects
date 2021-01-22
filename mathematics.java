a6a29983964e587b05f348256a2e457512ff99fd
patelhrid <74324750+patelhrid@users.noreply.github.com>
2021-01-21 21:54:08 -0500

    Add files via upload

---
+++ mathematics.java
@@ -0,0 +1,16 @@
+class mathematics
+{
+  public static void main (String args[])
+  {
+    System.out.println(2+7/2);
+    System.out.println(5*(6+3.0));
+    System.out.println(3*5/2);
+    System.out.println(2*(3/4));
+    System.out.println(3*4/5);
+    System.out.println(2.0+(7/(3/4)));
+    System.out.println(35/(5/4));
+    System.out.println(4-(6-(2-7)));
+    System.out.println(3/4*2.0);
+    System.out.println(3/(4*2.0));
+  }
+}
