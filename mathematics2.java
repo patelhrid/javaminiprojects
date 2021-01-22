9159dc6ecc9cef67f6880aff26ce2a14aeb835c7
patelhrid <74324750+patelhrid@users.noreply.github.com>
2021-01-21 21:54:45 -0500

    Add files via upload

---
+++ mathematics2.java
@@ -0,0 +1,10 @@
+class mathematics2
+{
+  public static void main (String args[])
+  {
+    System.out.println((2+3)/(2-3));
+    System.out.println(5+3*(2/4.0));
+    System.out.println((3+5)/2+(7/(5-2)));
+    System.out.println((2.0+(3.0/4.0))/(7.0-3.0));
+  }
+}
